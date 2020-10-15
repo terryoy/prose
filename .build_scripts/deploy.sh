effcf17832a27e610350d1045447acb72a1a8d64
Terry Ouyang <terry.ouyang@gmail.com>
2020-10-15 22:24:11 +0800

    refactor: re-organize static files and add github actions

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+FINAL_PATH=public
+
 # If this is the deploy branch, push it up to gh-pages
 echo "${DEPLOY_BRANCH}"
 echo "${GH_REF}"
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
-  rm -rf site
-  mkdir site
-  cp -a dist site
-  cp -a img fonts index.html style-rtl.css locale.js oauth.json CNAME translations site
-  cd site
+  rm -rf e
+  mkdir $FINAL_PATH
+  cp -a dist $FINAL_PATH
+  #cp -a img fonts index.html style-rtl.css locale.js oauth.json CNAME translations $FINAL_PATH
+  cp -a site/* $FINAL_PATH
+  cd $FINAL_PATH
   git init
-  git config user.name "Travis-CI"
-  git config user.email "travis@somewhere.com"
+  git config user.name "Github-CI"
+  git config user.email "actions@github.com"
   git add .
   git commit -m "CI deploy to gh-pages"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
